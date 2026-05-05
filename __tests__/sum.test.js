2da76ca947369b6de11c412202008f7e5e29e78f
Pranav <prganesan@ucsd.edu>
2026-05-05 15:44:41 -0700

    intentionally breaking master for failure screenshot

--- __tests__/sum.test.js
+++ __tests__/sum.test.js
@@ -1,14 +1,5 @@
-<<<<<<< HEAD
 import { sum } from '../sum.js';
 
 test('adds 1 + 2 to equal 3', () => {
-  // We use 4 so the GitHub Action fails
-=======
-// sum.test.js
-import { sum } from '../sum.js';
-
-test('adds 1 + 2 to equal 3', () => {
-  // We change the expectation to 4 so that the test fails for the screenshot requirement
->>>>>>> d9762f0d60b30bef1a1bd447953a19326b310e68
-  expect(sum(1, 2)).toBe(4);
+  expect(sum(1, 2)).toBe(4); // Wrong answer to force failure
 });
