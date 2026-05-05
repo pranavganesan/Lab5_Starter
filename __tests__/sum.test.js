e826d9fc302e286f0b2b263db18edead4713d2f6
Pranav <prganesan@ucsd.edu>
2026-05-05 15:12:19 -0700

    intentionally breaking test for screenshot

--- __tests__/sum.test.js
+++ __tests__/sum.test.js
@@ -1,5 +1,7 @@
 // sum.test.js
+import { sum } from '../sum.js';
 
 test('adds 1 + 2 to equal 3', () => {
-  // TODO
+  // We change the expectation to 4 so that the test fails for the screenshot requirement
+  expect(sum(1, 2)).toBe(4);
 });
