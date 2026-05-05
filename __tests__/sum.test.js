d6e84d4696d90af9efc4dc28d56de41b5bde1d67
Pranav <prganesan@ucsd.edu>
2026-05-05 15:54:10 -0700

    fix: restore passing test for final submission

--- __tests__/sum.test.js
+++ __tests__/sum.test.js
@@ -1,5 +1,5 @@
 import { sum } from '../sum.js';
 
 test('adds 1 + 2 to equal 3', () => {
-  expect(sum(1, 2)).toBe(4); // Wrong answer to force failure
+  expect(sum(1, 2)).toBe(3); // Change 4 back to 3
 });
